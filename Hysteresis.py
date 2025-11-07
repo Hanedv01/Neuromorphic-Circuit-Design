556f0dd6381192e2d02d3d89da34876640a09791
Hanedv01 <hannes_tjulin@hotmail.com>
2025-11-07 11:34:46 +0100

    Added shift and decay

--- Hysteresis.py
+++ Hysteresis.py
@@ -5,19 +5,24 @@ class AFE_FET:
     current = 0
     voltage = 0
     stateOn = False
-    def __init__(self, Vstart, Vsat, Isat, Roff, Ron, Vhl, Vlh):
-        self.Vstart = Vstart
-        self.Vsat = Vsat
-        self.Isat = Isat
+    SpikeTimes = []
+    def __init__(self, Vstart, Vsat, Isat, Roff, Ron, Vhl, Vlh, tau, deltaV, deltaI):
+        self.Vstart0 = Vstart
+        self.Vsat0 = Vsat
+        self.Isat0 = Isat
         self.Roff = Roff
         self.Ron = Ron
-        self.Vhl = Vhl
-        self.Vlh = Vlh
-        if self.Vstart > self.Vhl:
+        self.Vhl0 = Vhl
+        self.Vlh0 = Vlh
+        self.tau = tau
+        self.deltaV = deltaV
+        self.deltaI = deltaI
+
+        if self.Vstart0 > self.Vhl0:
             raise ValueError("Vstart should be smaller than Vhl!")
-        if self.Vhl > self.Vsat:
+        if self.Vhl0 > self.Vsat0:
             raise ValueError("Vhl should be smaller than Vsat!")
-        if self.Vlh > self.Vsat:
+        if self.Vlh0 > self.Vsat0:
             raise ValueError("Vlh should be smaller than Vsat!")
 
     def getSlopeUp(self):
@@ -25,7 +30,6 @@ class AFE_FET:
     
     def getSlopeDown(self):
         return (self.Line(self.Vlh-self.Vsat, 1/self.Ron, self.Isat)-self.Vstart/self.Roff)/(self.Vlh-self.Vstart)
-    
     
     
     # ------------------------------------------------------------------------------
@@ -36,44 +40,94 @@ class AFE_FET:
     def Line(self, V, slope, intersect):
         return slope*V + intersect
     
+    def AddSpike(self, tSpike):
+        self.SpikeTimes = np.append(self.SpikeTimes, tSpike)
 
-    def Update(self, Vnew):
+    def Shift(self, t, delta, tau):
+        output = 0
+        for tSpike in self.SpikeTimes:
+            if t >= tSpike:
+                output += delta * np.exp(-(t-tSpike) / tau)
+        return output
+
+    # Gives the new current, given past state and new voltage
+    def Update(self, Vnew, t):
         self.Vold = self.voltage
         self.voltage = Vnew
+        # Update voltages and currents according to shift
+        Vstart = self.Vstart0 + self.Shift(t, self.deltaV, self.tau)
+        Vsat   = self.Vsat0   + self.Shift(t, self.deltaV, self.tau)
+        Isat   = self.Isat0   + self.Shift(t, self.deltaI, self.tau)
+        Vhl    = self.Vhl0    + self.Shift(t, self.deltaV, self.tau)
+        Vlh    = self.Vlh0    + self.Shift(t, self.deltaV, self.tau)
+        print(Vhl)
         # Corresponding to the baseline through the origin
-        if Vnew <= self.Vstart:
+        if Vnew <= Vstart:
             self.current = self.Line(Vnew, 1/self.Roff, 0)
-            if not self.stateOn:
+            if self.stateOn:
                 self.stateOn = False
-                # One loop is done! Update V and I!
+                self.AddSpike(t)
         # Correponding to the saturated current after Vsat
-        elif Vnew >= self.Vsat:
-            self.current = self.Line(Vnew-self.Vsat, 1/self.Ron, self.Isat)
+        elif Vnew >= Vsat:
+            self.current = self.Line(Vnew-Vsat, 1/self.Ron, Isat)
             self.stateOn = True
         # Corresponding to the loop
         else:
             if not self.stateOn:
-                if Vnew <= self.Vhl:
-                    self.current = self.Line(Vnew, 1/self.Roff, 0)
+                if Vnew <= Vhl:
+                    self.current = self.Line(Vnew, 1/self.Roff, 0) + self.Shift(t, self.deltaI, self.tau)
                 else:
                     if Vnew >= self.Vold:
-                        self.current = self.Line(Vnew-self.Vhl, (self.Isat - self.Vhl/self.Roff)/(self.Vsat-self.Vhl), self.Vhl/self.Roff)
+                        self.current = self.Line(Vnew-Vhl, (Isat - Vhl/self.Roff)/(Vsat-Vhl), Vhl/self.Roff)
                     else:
-                        self.current = self.Line(Vnew-self.Vsat, 1/self.Ron, self.Isat)
+                        self.current = self.Line(Vnew-Vsat, 1/self.Ron, Isat)
                         self.stateOn = True
             else:
-                if Vnew >= self.Vlh:
-                    self.current = self.Line(Vnew-self.Vsat, 1/self.Ron, self.Isat)
+                if Vnew >= Vlh:
+                    self.current = self.Line(Vnew-Vsat, 1/self.Ron, Isat)
                 else:
                     if Vnew <= self.Vold:
-                        self.current = self.Line(Vnew-self.Vstart, (self.Line(self.Vlh-self.Vsat, 1/self.Ron, self.Isat)-self.Vstart/self.Roff)/(self.Vlh-self.Vstart), self.Vstart/self.Roff)
+                        self.current = self.Line(Vnew-Vstart, (self.Line(Vlh-Vsat, 1/self.Ron, Isat)-Vstart/self.Roff)/(Vlh-Vstart), Vstart/self.Roff)
                         self.stateOn = True
 
         
+"""
+class TestCicuit:
+    current = 0
+    time = 0
+    shift = 0
+    SpikeTimes = np.array([])
+    def __init__(self, tau):
+        self.tau = tau
 
+    def AddSpike(self, tSpike):
+        self.SpikeTimes = np.append(self.SpikeTimes, tSpike)
 
+    def Shift(self, t, delta):
+        output = 0
+        for tSpike in self.SpikeTimes:
+            if t >= tSpike:
+                output += delta * np.exp(-(t-tSpike) / self.tau)
+        return output
+         
 
-x = AFE_FET(1, 7, 100, 1e3, 1e3, 5, 3)
+tlist = np.linspace(0,1,1000)
+
+circuit = TestCicuit(0.1)
+Ilist = []
+for ind in range(len(tlist)):
+    if ind%200 == 0 and ind<=500:
+        circuit.AddSpike(tlist[ind])
+    Ilist.append(circuit.Shift(tlist[ind], 2))
+    print(f"{tlist[ind]},   {Ilist[-1]},   {circuit.SpikeTimes}")
+
+plt.plot(tlist, Ilist)
+plt.show()
+
+"""
+
+x = AFE_FET(1, 7, 100, 1e3, 1e3, 5, 3, 100, 0.1, 5)
+
 TP = 8
 Alist = np.linspace(0, TP, 100)
 Blist = np.linspace(TP, 0, 100)
@@ -81,11 +135,15 @@ Vlist = np.concatenate((Alist, Blist, Alist, Blist))
 #Vlist = [0,1,2,3,4,5,6,7,8,7,6,5,4,3,2,1,0]
 #Vlist = [1,3,2,8,4,6,3,7,3,1]
 Ilist = []
+tlist = [0]
 for V in Vlist:
-    x.Update(V)
+    t = tlist[-1] + 0.1
+    x.Update(V, t)
     Ilist.append(x.current)
+
+print(x.SpikeTimes)
 
 #print(Ilist)
 plt.close()
-plt.scatter(Vlist, Ilist)
+plt.plot(Vlist, Ilist)
 plt.show()
